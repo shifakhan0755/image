8407db12c755a97924089ca31341cdab2df5930e
shifakhan0755 <70809018+shifakhan0755@users.noreply.github.com>
2021-03-13 13:05:06 +0530

    Update settings.py

--- imageuploader/settings.py
+++ imageuploader/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 't&%d-y%bp8c*+mkrokm3vndi1%=990mpg!j(u@pq&hh@$fh3h5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['imageuploader11.herokuapp.com','127.0.0.1']
 
 
 # Application definition
