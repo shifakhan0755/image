ffc10f6323d165bc56a661423daa494341f450ae
shifakhan0755 <70809018+shifakhan0755@users.noreply.github.com>
2022-08-24 13:08:09 +0530

    Update urls.py

--- imageuploader/urls.py
+++ imageuploader/urls.py
@@ -14,13 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from home import views
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home),
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-print(static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT))
+    re_path(r'^media/(?P<path>.*)$',serve,{'document_root': settings.MEDIA_ROOT}),
+] 
+# + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# print("dddddd",static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT))
